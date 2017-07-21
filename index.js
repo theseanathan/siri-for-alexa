eae2ac21ec7900c06cc3d0d5250dc156723989f0
Alex Hsieh <alexander-d-hsieh@github.com>
2017-07-21 12:48:17 -0400

    Saving intents.json to output directory

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var alexa = require('alexa-app');
 var app = new alexa.app('my_macros');
 var onboard = require('./intents/onboard.js');
+var fs = require('fs');
+
+
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 1;
 module.exports = app;
@@ -96,7 +99,10 @@ app.error = function(e, request, response) {
   response.say("I captured the exception! It was: " + e.message);
 };
 
-console.log(app.schema());
-
+fs.writeFile('output/intents.json', app.schema(), function(err) {
+    if (err) {
+        console.log(err)
+    }
+})
 //Contact GitHub API Training Shop Blog About
 //© 2017 GitHub, Inc. Terms Privacy Security Status Help
